b0c53a885a504a886e75cc22ae4df804b1e9e50d
Doc0160 <t0160d@gmail.com>
2016-05-23 19:48:08 +0200

    Cloud shot missiles

--- src/jpu2016/dogfight/model/Missile.java
+++ src/jpu2016/dogfight/model/Missile.java
@@ -1,5 +1,24 @@
 package jpu2016.dogfight.model;
 
 public class Missile {
-
+	private int SPEED = 4;
+	private int WEIGHT = 30;
+	private int HEIGHT = 10;
+	private int MAX_DISTANCE_TRAVELED = 1400;
+	private String IMAGE = "missile";
+	public Missile(Direction direction, Dimension dimension){
+		
+	}
+	public int getWidthWithADirection(Direction direction){
+		return -1;
+	}
+	public int getHeightWithADirection(Direction direction){
+		return -1;
+	}
+	public void move(){
+		
+	}
+	public boolean isWeapon(){
+		return false;
+	}
 }
