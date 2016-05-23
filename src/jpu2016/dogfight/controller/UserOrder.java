595d5056326f204b9383c6634fcec51ee23c1094
lucasnabas <lucasnabas@10.151.128.14>
2016-05-23 17:01:25 +0200

    Modif controlleur (order, userorder)

--- src/jpu2016/dogfight/controller/UserOrder.java
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -1,8 +1,17 @@
 package jpu2016.dogfight.controller;
 
-public class UserOrder 
+public class UserOrder implements IUserOrder
 {
 	private int player;
+	private Order order;
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
 
 	public int getPlayer() {
 		return player;
