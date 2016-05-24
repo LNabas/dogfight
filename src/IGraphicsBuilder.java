419431cc3ca902ac1f123edfab6d528a053f0849
Rhum <rhum@10.151.128.30>
2016-05-24 11:01:29 +0200

    View.fini

---
+++ src/IGraphicsBuilder.java
@@ -0,0 +1,4 @@
+
+public interface IGraphicsBuilder {
+
+}
--- src/jpu2016/dogfight/view/DogfightView.java
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -1,11 +1,11 @@
 package jpu2016.dogfight.view;
 
 import java.util.Observable;
-
 import jpu2016.dogfight.controller.IOrderPerformer;
 import jpu2016.dogfight.model.IDogfightModel;
 
-public class DogfightView implements IViewSystem{
+public class DogfightView implements IViewSystem 
+{
 	public DogfightView (IOrderPerformer orderPerformer, IDogfightModel dogfightModel, Observable observable){
 		
 	}
