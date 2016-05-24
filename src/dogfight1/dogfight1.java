7cd94bd466610801510f809aff83d54c455f8502
lucasnabas <lucasnabas@10.151.128.14>
2016-05-24 10:48:55 +0200

    Main controller

--- src/dogfight1/dogfight1.java
+++ src/dogfight1/dogfight1.java
@@ -1,9 +1,18 @@
 package dogfight1;
 
+import jpu2016.dogfight.controller.DogfightController;
+import jpu2016.dogfight.model.DogfightModel;
+import jpu2016.dogfight.view.DogfightView;
+
 public class dogfight1
 {
 	public static void main()
 	{
-	
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel,
+		dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
+		dogfightController.play(); 
 	}
 }
